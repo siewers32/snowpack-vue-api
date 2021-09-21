d804188ffb2ad2be47d62701724649583de66f82
janjaap <janjaap@roc-dev.com>
2021-09-21 20:37:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3 class="p-3 text-center">Personen ophalen uit API</h3>
+  <get-request/>
+</template>
+
+<script>
+import GetRequest from "./GetRequest";
+
+export default {
+  name: "app",
+  components: {
+    GetRequest,
+  }
+};
+</script>
+
+
+
+
+
+
+
---
+++ src/GetRequest.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card text-center m-3">
+    <h5 class="card-header">Geef personen weer...</h5>
+    <ul id="array-rendering">
+      <li v-for="person in persons">
+        {{ person.first_name }}, {{ person.last_name }}, {{ person.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "get-request",
+  data() {
+    return {
+      persons: null
+    };
+  },
+  created() {
+    // Simple GET request using fetch
+    fetch('http://127.0.0.1:8888/api/person/search/a', {mode: 'cors'})
+        .then(response => response.json())
+        .then(data => (this.persons = data.data))
+        .catch(error => (console.log('Request failed', error)));
+    }
+};
+</script>
+
+<style>
+  ul {
+    background-color:cadetblue;
+  }
+</style>
